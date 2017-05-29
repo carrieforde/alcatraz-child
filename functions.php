6537c0a300d7bbbefbca3a8c5b4137425752c3d1
Carrie Forde <carrie@carrieforde.com>
2017-05-29 08:52:53 -0700

    Adds Google font to child theme

--- functions.php
+++ functions.php
@@ -13,10 +13,22 @@ add_action( 'wp_enqueue_scripts', 'alcatraz_child_enqueue_scripts', 15 );
  */
 function alcatraz_child_enqueue_scripts() {
 
+	$google_fonts = '//fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro:400,400i,700,700i';
+	$google_fonts = str_replace( ',', '%2C', $google_fonts );
+	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+	// Google fonts.
+	wp_enqueue_style(
+		'google-fonts',
+		$google_fonts,
+		array(),
+		ALCATRAZ_CHILD_VERSION
+	);
+
 	// Include this theme's stylesheet.
 	wp_enqueue_style(
 		'alcatraz-child-style',
-		ALCATRAZ_CHILD_URL . 'style.css',
+		ALCATRAZ_CHILD_URL . 'style' . $min . '.css',
 		array(),
 		ALCATRAZ_CHILD_VERSION
 	);
@@ -24,7 +36,7 @@ function alcatraz_child_enqueue_scripts() {
 	// Include this theme's JS.
 	wp_enqueue_script(
 		'alcatraz-child-scripts',
-		ALCATRAZ_CHILD_URL . 'js/alcatraz-child-theme.min.js',
+		ALCATRAZ_CHILD_URL . 'assets/scripts/alcatraz-child-theme' . $min . 'js',
 		array( 'jquery' ),
 		ALCATRAZ_CHILD_VERSION
 	);
